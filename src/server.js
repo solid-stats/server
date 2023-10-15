02870826bb44447f9319b3c7fe6db484c5a9e4be
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2023-10-15 17:24:19 +0700

    Improved paths declaring

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const os = require("os");
+const os = require('os');
+const path = require('path');
 
 const app = express();
 
+const listsPath = path.join('sg_stats', 'lists');
+
 app.get('/maces_list', (req, res) => {
-  res.sendFile('sg-replay-parser/mace_list.html', { root: os.homedir() })
+  res.sendFile(path.join(listsPath, 'mace_list.html'), { root: os.homedir() });
 });
 
 app.get('/mission_makers_list', (req, res) => {
-  res.sendFile('sg-replay-parser/mission_makers_list.html', { root: os.homedir() })
+  res.sendFile(path.join(listsPath, 'mission_makers_list.html'), { root: os.homedir() });
 });
 
 app.listen(7001);
