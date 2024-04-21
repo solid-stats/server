f67cb6dcdd26e0d478155beae66087180f307f60
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2024-04-21 19:15:52 +0700

    Added route for full rotation squad info

--- src/routes/globalStats.js
+++ src/routes/globalStats.js
@@ -8,6 +8,7 @@ const router = new Router();
 
 const playersGlobalStatisticsFileName = 'global_statistics.json';
 const squadsGlobalStatisticsFileName = 'squad_statistics.json';
+const squadsFullRotationFileName = 'squad_full_rotation_statistics.json';
 
 router.get('/players/:game_type', (req, res) => {
   const filePath = isGameTypeWithRotations(req.params.game_type)
@@ -53,6 +54,17 @@ router.get('/squads/:game_type/rotation/:rotation_number', (req, res) => {
       req.params.game_type,
       getRotationFolderName(req.params.rotation_number),
       squadsGlobalStatisticsFileName,
+    ),
+  );
+});
+
+router.get('/squads_full_rotation/:game_type/rotation/:rotation_number', (req, res) => {
+  res.sendFile(
+    path.join(
+      resultsPath,
+      req.params.game_type,
+      getRotationFolderName(req.params.rotation_number),
+      squadsFullRotationFileName,
     ),
   );
 });
